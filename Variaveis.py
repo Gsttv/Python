9796002a057ef71063be8c00c25696c81d743234
Gsttv <gustvmorais@gmail.com>
2022-08-19 11:18:45 -0300

    feature/ Variables

---
+++ Variaveis.py
@@ -0,0 +1,5 @@
+# Variaveis
+salario_mensal = input('Qual o valor do seu salario mensal')
+horas_trabalhadas = input('Quais sao suas horas trabalhadas no mes')
+valor_hora = int(salario_mensal)/int(horas_trabalhadas)
+print(valor_hora)
