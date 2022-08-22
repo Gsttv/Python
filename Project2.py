11a0efd80ca24ae79179718a5c93a9b4a2938ec5
Gsttv <gustvmorais@gmail.com>
2022-08-22 11:10:10 -0300

    feature project 2

--- Project2.py
+++ Project2.py
@@ -1,18 +1,24 @@
 # Random number
 
 import random
-numero = int(input("Digite um numero de 1 a 10"))
 valor_aleatorio = random.randint(1,10)
 
-if numero>=1 and numero<=10:
-    if numero>valor_aleatorio:
-        print("O numero escolhido é maior que o valor gerado")
-        print(valor_aleatorio)
-    elif numero<valor_aleatorio:
-        print("o o numero escolhido é menor que o valor gerado")
-        print(valor_aleatorio)
-    elif numero == valor_aleatorio:
-        print(" O numero escolhido é igual ao valor gerado")
+acertou = False
 
-else:
-    print("O numedo escolhido deve estar entre 1 e 10 ")
+
+while acertou == False:
+        numero = int(input("Digite um numero de 1 a 10"))
+
+        if numero > valor_aleatorio:
+           print("O numero escolhido é maior que o valor gerado")
+
+
+        elif numero<valor_aleatorio:
+            print("o o numero escolhido é menor que o valor gerado")
+
+
+        elif numero == valor_aleatorio:
+            acertou = True
+            print(" O numero escolhido é igual ao valor gerado")
+
+
